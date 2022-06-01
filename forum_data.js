d789571114197deb261327acea0f83339fb9f134
AsibeHalacheva1703 <62263931+AsibeHalacheva1703@users.noreply.github.com>
2022-06-01 22:57:33 +0300

    forum

---
+++ forum_data.js
@@ -0,0 +1,40 @@
+var threads = [
+    {
+        id: 1,
+        title: "Тема 1",
+        author: "Admin",
+        date: Date.now(),
+        content: "Събиране на випуск 2015",
+        comments: [
+            {
+                author: "Виктория",
+                date: Date.now(),
+                content: "Ще присъствам"
+            },
+            {
+                author: "Тодор",
+                date: Date.now(),
+                content: "Там съм"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Тема 2",
+        author: "Admin",
+        date: Date.now(),
+        content: "Събиране на випуск 2016",
+        comments: [
+            {
+                author: "Никола",
+                date: Date.now(),
+                content: "Няма да мога да дойда."
+            },
+            {
+                author: "Ана",
+                date: Date.now(),
+                content: "Ще присъствам"
+            }
+        ]
+    }
+]
