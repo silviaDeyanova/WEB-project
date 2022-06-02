c21ba329c95c67727dc0408a4c3839b33c822787
silviaDeyanova <63786822+silviaDeyanova@users.noreply.github.com>
2022-06-02 10:21:46 +0300

    styles and routing to login page

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-window.location.replace("./front-end/login.html")
+const btn = document.querySelector("button");
+
+const login = (event) => {
+  event.preventDefault();
+  console.log(event);
+  window.location.href = "front-end/login/login.html";
+};
+btn.addEventListener("click", login);
