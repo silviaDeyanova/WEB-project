e8713ca89b3e08976ba7d319a0dc759915242494
AsibeHalacheva1703 <62263931+AsibeHalacheva1703@users.noreply.github.com>
2022-06-07 12:15:10 +0300

    new db

--- back-end/db/create/alumni.sql
+++ back-end/db/create/alumni.sql
@@ -67,6 +67,63 @@ ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE categories (
+cat_id INT(8) NOT NULL AUTO_INCREMENT,
+cat_name VARCHAR(255) NOT NULL,
+cat_description VARCHAR(255) NOT NULL,
+UNIQUE INDEX cat_name_unique (cat_name),
+PRIMARY KEY (cat_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topics`
+--
+
+CREATE TABLE topics (
+topic_id INT(8) NOT NULL AUTO_INCREMENT,
+topic_subject VARCHAR(255) NOT NULL,
+topic_date DATETIME NOT NULL,
+topic_cat INT(8) NOT NULL,
+topic_by INT(8) NOT NULL,
+PRIMARY KEY (topic_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- link topics to categories
+
+ALTER TABLE topics ADD FOREIGN KEY(topic_cat) REFERENCES categories(cat_id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- topics to user
+ALTER TABLE topics ADD FOREIGN KEY(topic_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE posts (
+post_id INT(8) NOT NULL AUTO_INCREMENT,
+post_content TEXT NOT NULL,
+post_date DATETIME NOT NULL,
+post_topic INT(8) NOT NULL,
+post_by INT(8) NOT NULL,
+PRIMARY KEY (post_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- posts to topic
+ALTER TABLE posts ADD FOREIGN KEY(post_topic) REFERENCES topics(topic_id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--post to user
+ALTER TABLE posts ADD FOREIGN KEY(post_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
