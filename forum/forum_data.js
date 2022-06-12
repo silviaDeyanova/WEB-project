d914cd7c3b41e50ec88044b18526ac4e26a3de65
AsibeHalacheva1703 <62263931+AsibeHalacheva1703@users.noreply.github.com>
2022-06-13 00:37:26 +0300

    nav-bars

--- forum/forum_data.js
+++ forum/forum_data.js
@@ -1,8 +1,8 @@
 var defaultThreads = [
     {
         id: 1,
-        title: "Събиране на випуск 2015",
-        author: "Stanislava",
+        title: "Среща на випуск 2015",
+        author: "Силвия",
         date: Date.now(),
         content: "Срещата ще се състои на 25.07.2022г. В ресторант Панорама от 20:00ч.",
         comments: [
@@ -21,7 +21,7 @@ var defaultThreads = [
     {
         id: 2,
         title: "Почистване на кв. Студенски град",
-        author: "Asibe",
+        author: "Станислава",
         date: Date.now(),
         content: "Почистване на кв. Студенски град на 20.07.2022г.",
         comments: [
@@ -36,7 +36,26 @@ var defaultThreads = [
                 content: "Къде да видим подробности?"
             }
         ]
-    }
+    },
+    {
+        id: 3,
+        title: "Тържествено връчване на дипломите",
+        author: "Асибе",
+        date: Date.now(),
+        content: "На 19.04.2022 г. (вторник) от 14.00 ч. в Аулата на Софийски университет",
+        comments: [
+            {
+                author: "Никола",
+                date: Date.now(),
+                content: "Там сме."
+            },
+            {
+                author: "Ана",
+                date: Date.now(),
+                content: "Къде да видим подробности?"
+            }
+        ]
+    },  
 ]
 
 var threads = defaultThreads
@@ -49,4 +68,4 @@ if (localStorage && localStorage.getItem('threads')) {
 
 
 
-//window.localStorage.clear();
+window.localStorage.clear();
