fe921c5fe42bb2c9d3204c116058976612b67226
stnssllv <106099288+stnssllv@users.noreply.github.com>
2022-06-13 02:10:51 +0300

    new try

--- back-end/api/profile/profile.php
+++ back-end/api/profile/profile.php
@@ -6,19 +6,18 @@ $db = new DB();
 $connection = $db->getConnection();
 
 if ($username !== "") {    
-	$selectQ = "SELECT username,password,full_name,fn,email,graduation,major,groupN FROM users WHERE username = :username";
-
-    $stmt = $connection->prepare($selectQ);
-    $stmt->execute(["username" => $username]);
-    $user_data = $stmt->fetch(PDO::FETCH_ASSOC);  
+  $selectQ = "SELECT username,password,full_name,fn,email,graduation,major,groupN FROM users WHERE username = :username";
+  $stmt = $connection->prepare($selectQ);
+  $stmt->execute(["username" => $username]);
+  $user_data = $stmt->fetch(PDO::FETCH_ASSOC);  
   
-    $password = $user_data["password"];
-    $full_name = $user_data["full_name"];
-    $fn = $user_data["fn"];
-    $email = $user_data["email"];
-    $graduation = $user_data["graduation"];
-    $major = $user_data["major"];
-    $groupN = $user_data["groupN"];
+  $password = $user_data["password"];
+  $full_name = $user_data["full_name"];
+  $fn = $user_data["fn"];
+  $email = $user_data["email"];
+  $graduation = $user_data["graduation"];
+  $major = $user_data["major"];
+  $groupN = $user_data["groupN"];
 }
 
 $result = array("$password", "$full_name", "$fn", "$email", "$graduation", "$major", "$groupN");
