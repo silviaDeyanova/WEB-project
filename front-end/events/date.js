2ac26c0b00ebdb178ae9208d74e50862ec34adc6
AsibeHalacheva1703 <62263931+AsibeHalacheva1703@users.noreply.github.com>
2022-06-13 11:11:50 +0300

    new

---
+++ front-end/events/date.js
@@ -0,0 +1,7 @@
+<script>
+              date = new Date();
+              year = date.getFullYear();
+              month = date.getMonth() + 1;
+              day = date.getDate();
+              document.getElementById("current_date").innerHTML = month + "/" + day + "/" + year;
+</script> 
