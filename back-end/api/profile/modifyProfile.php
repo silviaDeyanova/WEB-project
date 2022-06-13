bcd903a1f071dc0a94fe2b9293739a1223b6ebca
stnssllv <106099288+stnssllv@users.noreply.github.com>
2022-06-13 09:32:29 +0300

    add updateUserQuery

--- back-end/api/profile/modifyProfile.php
+++ back-end/api/profile/modifyProfile.php
@@ -8,6 +8,23 @@ $phpInput = json_decode(file_get_contents('php://input'), true);
     $lastName = $phpInput['lastName'];
     $email = $phpInput['email'];
 
+    public function updateUserQuery($data)
+        {
+            $this->database->getConnection()->beginTransaction();   
+            try {
+                $sql = "UPDATE users SET password = :password, firstName = :firstName, 
+                        lastName = :lastName, email = :email WHERE id = '{$_SESSION['userId']}'";
+                $this->updateUser = $this->database->getConnection()->prepare($sql);
+                $this->updateUser->execute($data);
+                $this->database->getConnection()->commit();   
+                return ["success" => true];
+            } catch (PDOException $e) {
+                echo "exception test";
+                $this->database->getConnection()->rollBack();
+                return ["success" => false, "error" => "Connection failed: " . $e->getMessage()];
+            }
+        }
+
     public function updateUser($user)
         {
             $this->validate($user->password, $user->email, $user->firstName, $user->lastName);
