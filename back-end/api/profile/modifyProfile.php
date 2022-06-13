983b92fb0949ab50285d40c2d56578c0d403ed26
stnssllv <106099288+stnssllv@users.noreply.github.com>
2022-06-13 09:31:36 +0300

    add updateUser

--- back-end/api/profile/modifyProfile.php
+++ back-end/api/profile/modifyProfile.php
@@ -8,6 +8,16 @@ $phpInput = json_decode(file_get_contents('php://input'), true);
     $lastName = $phpInput['lastName'];
     $email = $phpInput['email'];
 
+    public function updateUser($user)
+        {
+            $this->validate($user->password, $user->email, $user->firstName, $user->lastName);
+            $query = $this->userRepository->updateUserQuery([
+                "password" => $user->password,
+                "firstName" => $user->firstName,
+                "lastName" => $user->lastName,
+                "email" => $user->email
+            ]);
+        }
    
     try {
         $userService->updateUser($user);
