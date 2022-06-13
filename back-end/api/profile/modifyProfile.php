34529640a2e2d59df5eca2df033f7c238f0ab12c
silviaDeyanova <63786822+silviaDeyanova@users.noreply.github.com>
2022-06-13 09:57:00 +0300

    edit

--- back-end/api/profile/modifyProfile.php
+++ back-end/api/profile/modifyProfile.php
@@ -4,23 +4,21 @@ require_once("../../db/connection/connect.php");
 $phpInput = json_decode(file_get_contents('php://input'), true);
 header('Content-Type: application/json');
 
-//$username = $phpInput['username'];
+$username = $phpInput['username'];
 $password = $phpInput['password'];
-//$fn = $phpInput['fn'];
-//$email = $phpInput['email'];
 $graduation = $phpInput['graduation'];
 $major = $phpInput['major'];
 $groupN = $phpInput['groupN'];
 
-
-$username = sessionStorage . getItem("username");
 $db = new DB();
 $connection = $db->getConnection();
 try {
-    $sql = "UPDATE users SET password = :password, graduation = :graduation, major = :major, groupN = :groupN WHERE username = '{$username}'";
+    $sql = "UPDATE users SET password = :password, graduation = :graduation, major = :major, groupN = :groupN WHERE username = :username";
     $updateUser = $connection()->prepare($sql);
-    $updateUser->execute(["password = :password", "graduation = :graduation", "major = :major", "groupN = :groupN"]);
+    $updateUser->execute(["password = :password", "graduation = :graduation", "major = :major", "groupN = :groupN", "username = :username"]);
     echo json_encode(["status" => "success", "message" => "Профилът ви беше променен успешно!"], JSON_UNESCAPED_UNICODE);
 } catch (PDOException $e) {
     return json_encode(["status" => "error", "message" => "Възникна грешка при промяната на профила!"], JSON_UNESCAPED_UNICODE);
 }
+
+?>
