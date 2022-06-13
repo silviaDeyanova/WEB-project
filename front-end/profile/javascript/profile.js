cc50cc64df8d5fc760fe252d4bfe1977f4503ac7
stnssllv <106099288+stnssllv@users.noreply.github.com>
2022-06-13 09:12:05 +0300

    c

--- front-end/profile/javascript/profile.js
+++ front-end/profile/javascript/profile.js
@@ -18,3 +18,66 @@ function GetInfo(usrname) {
     xmlhttp.send();
   }
 }
+
+const submitBtn = document.getElementById('submit-button');
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const password = document.getElementById("password");
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const email = document.getElementById("email");
+
+  if (!validateEmail(email.value)) {
+    showError(email, "Невалиден имейл адрес.");
+    return;
+  }
+  if (!validatePassword(password.value)) {
+    showError(password, "Невалидна парола.");
+    return;
+  }
+  if (!firstName.value || !lastName.value || !isBetween(firstName.value.length, 1, 50) || !isBetween(lastName.value.length, 1, 50)) {
+    showError(firstName, "Невалидно име или фамилия.");
+    return;
+  }
+  showSuccess(password, "Промените са запазени успешно.");
+  const formData = {
+    password: password.value,
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value
+  };
+
+  updateProfile(formData);
+});
+
+async function updateProfile(formData) {
+  const data = new FormData();
+
+  fetch('../../backend/endpoints/modifyProfile.php', {
+    method: 'PUT',
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Error updating profile info.');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.success === true) {
+        console.log("The profile is updated successfully.");
+      } else {
+        console.log('The profile is NOT updated successfully.');
+      }
+    })
+    .catch(error => {
+      const message = 'Error when updating profile.';
+      console.log(error);
+      console.error(message);
+    });
+}
