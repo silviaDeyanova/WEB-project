65274bf5969d76d2f3bb238d7355cf8e5544803b
silviaDeyanova <63786822+silviaDeyanova@users.noreply.github.com>
2022-06-13 11:33:54 +0300

    edit

--- back-end/api/profile/modifyProfile.php
+++ back-end/api/profile/modifyProfile.php
@@ -1,24 +1,32 @@
 <?php
 require_once("../../db/connection/connect.php");
 
-$phpInput = json_decode(file_get_contents('php://input'), true);
+header("Access-Control-Allow-Origin: *"); 
 header('Content-Type: application/json');
 
-$username = $phpInput['username'];
-$password = $phpInput['password'];
-$graduation = $phpInput['graduation'];
-$major = $phpInput['major'];
-$groupN = $phpInput['groupN'];
+$phpInput = json_decode(file_get_contents('php://input'), true);
+
+if (empty($phpInput['username']) || empty($phpInput['password'])) {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Моля, попълнете полетата!",
+    ], JSON_UNESCAPED_UNICODE);
+} else {
+    $username = $phpInput['username'];
+    $password = $phpInput['password'];
+    $graduation = $phpInput['graduation'];
+    $major = $phpInput['major'];
+    $groupN = $phpInput['groupN'];
 
 
-try {
-    $db = new DB();
-    $connection = $db->getConnection();
-    $sql = "UPDATE users SET password = :password, graduation = :graduation, major = :major, groupN = :groupN WHERE username = " . $username;
-    $updateUser = $connection()->prepare($sql);
-    $updateUser->execute(["password" => $password, "graduation" => $graduation, "major" => $major, "groupN" => $groupN]);
-    echo json_encode(["status" => "success", "message" => "Профилът ви беше променен успешно!"], JSON_UNESCAPED_UNICODE);
-    // echo json_encode(["status" => "success", "message" => $sql], JSON_UNESCAPED_UNICODE);
-} catch (PDOException $e) {
-    return json_encode(["status" => "error", "message" => "Възникна грешка при промяната на профила!"], JSON_UNESCAPED_UNICODE);
+    try {
+        $db = new DB();
+        $connection = $db->getConnection();
+        $sql = "UPDATE users SET password = :password, graduation = :graduation, major = :major, groupN = :groupN WHERE username = " . $username;
+        $updateUser = $connection->prepare($sql);
+        echo json_encode(["status" => "success", "message" => "Успешна промяна!"], JSON_UNESCAPED_UNICODE);
+        $updateUser->execute(["password" => $password, "graduation" => $graduation, "major" => $major, "groupN" => $groupN]);
+    } catch (PDOException $e) {
+        return json_encode(["status" => "error", "message" => "Възникна грешка при промяната на профила!"], JSON_UNESCAPED_UNICODE);
+    }
 }
--- front-end/profile/javascript/profile.js
+++ front-end/profile/javascript/profile.js
@@ -2,8 +2,12 @@ function GetInfo(usrname) {
   if (usrname.length != 0) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
-	  if (this.readyState == 4 && this.status == 200 && usrname == sessionStorage.getItem("username")) {
-        var myObj = JSON.parse(this.responseText);         
+      if (
+        this.readyState == 4 &&
+        this.status == 200 &&
+        usrname == sessionStorage.getItem("username")
+      ) {
+        var myObj = JSON.parse(this.responseText);
         document.getElementById("password").value = myObj[0];
         document.getElementById("full_name").innerHTML = myObj[1];
         document.getElementById("fn").value = myObj[2];
@@ -13,29 +17,33 @@ function GetInfo(usrname) {
         document.getElementById("groupN").value = myObj[6];
       }
     };
-  
-    xmlhttp.open("GET", "../../back-end/api/profile/profile.php?username=" + usrname, true);
+
+    xmlhttp.open(
+      "GET",
+      "../../back-end/api/profile/profile.php?username=" + usrname,
+      true
+    );
     xmlhttp.send();
   }
 }
 
 const submitBtn = document.getElementById("button");
 
-submitBtn.addEventListener('click', (event) => {
+submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
-	
+
   const username = document.getElementById("username");
   const password = document.getElementById("password");
   const graduation = document.getElementById("graduation");
   const major = document.getElementById("major");
   const groupN = document.getElementById("groupN");
-	
+
   const formData = {
     username: username.value,
     password: password.value,
     graduation: graduation.value,
     major: major.value,
-    groupN: groupN.value
+    groupN: groupN.value,
   };
 
   updateProfile(formData);
@@ -44,28 +52,24 @@ submitBtn.addEventListener('click', (event) => {
 async function updateProfile(formData) {
   const data = new FormData();
 
-  fetch('../../back-end/api/profile/modifyProfile.php', {
-    method: 'PUT',
+  fetch("../../back-end/api/profile/modifyProfile.php", {
+    method: "PUT",
     headers: {
       "Content-Type": "application/json; charset=utf-8",
     },
     body: JSON.stringify(formData),
   })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Error updating profile info.');
-      }
-      return response.json();
+    .then((res) => {
+      debugger
+      return res.json();
     })
     .then((data) => {
-      if (data.success === true) {
-        console.log("The profile is updated successfully.");
-      } else {
-        console.log('The profile is NOT updated successfully.');
+      if (data["status"] === "error") {
+        throw new Error(data["message"]);
       }
     })
-    .catch(error => {
-      const message = 'Error when updating profile.';
+    .catch((error) => {
+      const message = "Грешка при обновяване на профила";
       console.log(error);
       console.error(message);
     });
